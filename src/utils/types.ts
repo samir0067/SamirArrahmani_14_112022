28c09d2c370c76ede659e90b60f5506ba718e810
samir <arrahmani.samir@gmail.com>
2023-01-02 23:32:55 +0100

    wip types.ts

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,5 @@
-import * as hookForm from "react-hook-form";
 import { InputHTMLAttributes } from "react";
+import * as hookForm from "react-hook-form";
 
 export type VariantTypo =
   | "body1"
@@ -33,18 +33,22 @@ export type SelectProps = {
   options: { key: string; label: string }[];
 };
 
-export type Address = {
-  street: string;
-  zipCode: number;
-  city: string;
-  state: string;
-};
-
 export type Employee = {
   firstName: string;
   lastName: string;
   birthDate: string;
   startDate: string;
-  address: Address;
+  street: string;
+  zipCode: number;
+  city: string;
+  state: string;
   department: string;
+};
+
+export type HeadCell = {
+  id: keyof Employee;
+  padding: "none" | "normal" | "checkbox";
+  label: string;
+  align: "inherit" | "left" | "center" | "right" | "justify";
+  minWidth?: number;
 };
